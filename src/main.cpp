dacd832c9a3501b3a572e6de4c3053bd77b7c424
Eduardo Dominguez <Eduardo@altreact.com>
2026-09-12 16:55:11 -0400

    added code to test tft and touch screen functionality
    added black background then white words on top when screen wasnt being touched
    
    added green backgrond with red words on top while screen was being touched

---
+++ src/main.cpp
@@ -0,0 +1,46 @@
+#include <Arduino.h>
+#include <TFT_eSPI.h>
+#include <XPT2046_Touchscreen.h>
+#include <SPI.h> 
+
+TFT_eSPI tft = TFT_eSPI();
+XPT2046_Touchscreen ts = XPT2046_Touchscreen(33);
+bool wasTouched = false;
+
+void readyForTouch() {
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  tft.setTextFont(2); 
+  tft.drawCentreString("touch here to see if touch screen works", 160, 110, 2);
+}
+
+void setup() {
+  Serial.begin(115200);
+ 
+  // Wake up the screen
+  tft.init();
+  tft.setRotation(1); 
+  readyForTouch();
+
+  // 3. Route the hidden wires, then wake up the touch controller
+  SPI.begin(25, 39, 32, 33);
+  ts.begin();
+}
+
+void loop() {
+  // Ask our new touch object if the screen is being pressed
+  bool isTouched = ts.touched();
+
+  if (isTouched && !wasTouched) {
+    tft.fillScreen(TFT_GREEN);
+    tft.setTextColor(TFT_RED, TFT_GREEN);
+    tft.setTextFont(4); 
+    tft.drawCentreString("you touched me!", 160, 110, 4);
+    wasTouched = true;
+  }
+  
+  if (!isTouched && wasTouched) {
+    readyForTouch();
+    wasTouched = false;
+  }
+}
