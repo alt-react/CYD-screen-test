c2e0db2407b1a62109c5a8073793c9b7a5e30086
Eduardo Dominguez <Eduardo@altreact.com>
2026-09-12 16:51:16 -0400

    Add User_Setup.h for ILI9341 driver configuration
    define SPI pin for tft, SPI frequencies for tft and touch screen, and fonts used

---
+++ include/User_Setup.h
@@ -0,0 +1,18 @@
+#define ILI9341_DRIVER
+
+// CYD custom SPI pins
+#define TFT_MISO 12
+#define TFT_MOSI 13
+#define TFT_SCLK 14
+#define TFT_CS   15
+#define TFT_DC   2
+#define TOUCH_CS 33
+
+// Built-in fonts we requested
+#define LOAD_GLCD  
+#define LOAD_FONT2 
+#define LOAD_FONT4 
+
+// Standard SPI speeds
+#define SPI_FREQUENCY  27000000
+#define SPI_TOUCH_FREQUENCY  2500000
